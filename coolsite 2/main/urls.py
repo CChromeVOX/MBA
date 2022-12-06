11f5e66614af7e3575845165f9800df00d16545c
CChromeVOX <83388377+CChromeVOX@users.noreply.github.com>
2022-12-06 18:37:17 +0600

    Delete coolsite 2 directory

--- coolsite 2/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='main-index'),
-    path('about/', views.about, name='main-about'),
-    path('login/', views.login, name='main-login'),
-    path('lesson/', views.lesson, name='main-lesson'),
-]
